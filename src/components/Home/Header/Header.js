32856b16b4d76a519c114bddf13e33fd5bb8a48b
misbah Hasan <tahermisbah41@gmail.com>
2021-04-10 22:06:08 +0600

    some end

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import BusinessInfo from '../BusinessInfo/BusinessInfo';
-import HeaderMain from '../HeaderMain/HeaderMain';
-import Navbar from '../Navbar/Navbar';
-import './Header.css';
+import React from "react";
+import BusinessInfo from "../BusinessInfo/BusinessInfo";
+import HeaderMain from "../HeaderMain/HeaderMain";
+import Navbar from "../Navbar/Navbar";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className="header-container">
-            <Navbar></Navbar>
-            <HeaderMain></HeaderMain>
-            <BusinessInfo></BusinessInfo>
-        </div>
-    );
+  return (
+    <div className="header-container">
+      <div className="container">
+        <Navbar></Navbar>
+        <HeaderMain></HeaderMain>
+        <BusinessInfo></BusinessInfo>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import chair from '../../../images/chair.png';
+import React from "react";
+import chair from "../../../images/chair.png";
 
 const HeaderMain = () => {
-    return (
-        <main style={{height:'600px'}} className="row d-flex align-items-center">
-            <div className="col-md-4 offset-md-1">
-                <h1 style={{color: '#3A4256'}}>Your New Smile <br/> Starts Here</h1>
-                <p className="text-secondary">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore eveniet necessitatibus et iusto corrupti minima.</p>
-                <button className="btn btn-primary">GET APPOINTMENT</button>
-            </div>
-            <div className="col-md-6">
-                <img src={chair} alt="" className="img-fluid"/>
-            </div>
-        </main>
-    );
+  return (
+    <main style={{ height: "600px" }} className="row d-flex align-items-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 offset-md-1">
+            <h1 style={{ color: "#3A4256" }}>
+              Your New Smile <br /> Starts Here
+            </h1>
+            <p className="text-secondary">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+              eveniet necessitatibus et iusto corrupti minima.
+            </p>
+            <button className="btn btn-primary">GET APPOINTMENT</button>
+          </div>
+          <div className="col-md-6">
+            <img src={chair} alt="" className="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default HeaderMain;
